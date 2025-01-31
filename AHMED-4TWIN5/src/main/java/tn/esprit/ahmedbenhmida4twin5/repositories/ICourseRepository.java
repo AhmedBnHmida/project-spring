89c32ee5967f290d0dada02e820477167341345c
ahmed benhmida <100858528+axexp@users.noreply.github.com>
2025-01-31 23:55:41 +0100

    Jenkinsfile (Pipeline)

--- AHMED-4TWIN5/src/main/java/tn/esprit/ahmedbenhmida4twin5/repositories/ICourseRepository.java
+++ AHMED-4TWIN5/src/main/java/tn/esprit/ahmedbenhmida4twin5/repositories/ICourseRepository.java
@@ -1,11 +1,12 @@
 package tn.esprit.ahmedbenhmida4twin5.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.ahmedbenhmida4twin5.entities.Course;
 
-import java.util.List;
 
 
+@Repository
 public interface ICourseRepository extends JpaRepository<Course, Long> {
     Course findCourseBynumCourse(Long numCourse);
 }
